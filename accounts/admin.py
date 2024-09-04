97f2a32faea6323060b58fa6df5377c522c673cb
zila-tech <info.zilatech@gmail.com>
2024-09-04 14:47:30 +0000

    Update admin register with PrivacyPolicySection

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from accounts.forms import CustomUserChangeForm, CustomUserCreationForm
-from accounts.models import Account
 from django.utils.translation import gettext_lazy as _
+from .models import PrivacyPolicySection, PrivacyPolicySubSection, Account
 
 admin.site.site_header = _("Breast Cancer Prediction Administration")
 admin.site.site_title = _("Breast Cancer Prediction Admin Portal")
@@ -93,3 +93,39 @@ class AccountAdmin(UserAdmin):
     )
     search_fields = ("email", "username", "first_name", "last_name")
     ordering = ("email",)
+
+
+@admin.register(PrivacyPolicySection)
+class PrivacyPolicySectionAdmin(admin.ModelAdmin):
+    # List of fields to be displayed in the list view
+    list_display = ["title", "order"]
+
+    # Fields to be searched through the admin search bar
+    search_fields = ["title", "content"]
+
+    # Fields to filter by (if necessary)
+    list_filter = ["order"]
+
+    # Add pagination support (optional: this is automatic in Django but can be configured)
+    list_per_page = 10
+
+    # Ordering in the admin list view
+    ordering = ["order"]
+
+
+@admin.register(PrivacyPolicySubSection)
+class PrivacyPolicySubSectionAdmin(admin.ModelAdmin):
+    # List of fields to be displayed in the list view
+    list_display = ["section", "title", "order"]
+
+    # Fields to be searched through the admin search bar
+    search_fields = ["title", "content", "section__title"]
+
+    # Filters
+    list_filter = ["section"]
+
+    # Add pagination support
+    list_per_page = 10
+
+    # Ordering in the admin list view
+    ordering = ["section", "order"]
