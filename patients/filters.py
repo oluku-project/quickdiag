df219f250e340f0a7a861d428d49ffcb1133978c
zila-tech <info.zilatech@gmail.com>
2024-09-04 22:01:32 +0000

    Update filter with additional models and logics

--- patients/filters.py
+++ patients/filters.py
@@ -1,7 +1,10 @@
 import datetime
 import django_filters
 from django import forms
-from .models import STATE, Contact, PredictionResult
+
+from ml.models import ActivityLog, TrainedModel
+from patients.utils import RATE_CHOICES
+from .models import STATE, Contact, Feedback, PredictionResult
 from django.db.models import F, ExpressionWrapper, DurationField, IntegerField
 from django.db.models.functions import Now
 
@@ -261,22 +264,30 @@ class ContactFilter(django_filters.FilterSet):
         field_name="user__username",
         lookup_expr="icontains",
         label="User",
-        widget=forms.TextInput(attrs={"placeholder": "Search by Username"}),
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Username"}
+        ),
     )
     name = django_filters.CharFilter(
         lookup_expr="icontains",
         label="Name",
-        widget=forms.TextInput(attrs={"placeholder": "Search by Name"}),
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Name"}
+        ),
     )
     subject = django_filters.CharFilter(
         lookup_expr="icontains",
         label="Subject",
-        widget=forms.TextInput(attrs={"placeholder": "Search by Subject"}),
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Subject"}
+        ),
     )
     email = django_filters.CharFilter(
         lookup_expr="icontains",
         label="Email",
-        widget=forms.EmailInput(attrs={"placeholder": "Search by Email"}),
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Search by Email"}
+        ),
     )
     # Submission Date Start field
     submission_date_start = django_filters.DateFilter(
@@ -307,4 +318,252 @@ class ContactFilter(django_filters.FilterSet):
             "email",
             "submission_date_start",
             "submission_date_end",
+        ]
+
+
+class TrainedModelFilterFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="Name",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Name"}
+        ),
+    )
+    version = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="Version",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Version"}
+        ),
+    )
+    model_type = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="Model Type",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Model Type"}
+        ),
+    )
+    accuracy_min = django_filters.NumberFilter(
+        field_name="accuracy",
+        lookup_expr="gte",
+        label="Min Accuracy Score",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Min Accuracy Score"}
+        ),
+    )
+    accuracy_max = django_filters.NumberFilter(
+        field_name="accuracy",
+        lookup_expr="lte",
+        label="Max Accuracy Score",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Max Accuracy Score"}
+        ),
+    )
+    precision_min = django_filters.NumberFilter(
+        field_name="precision",
+        lookup_expr="gte",
+        label="Min Precision Score",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Min Precision Score"}
+        ),
+    )
+    precision_max = django_filters.NumberFilter(
+        field_name="precision",
+        lookup_expr="lte",
+        label="Max Precision Score",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Max Precision Score"}
+        ),
+    )
+    recall_min = django_filters.NumberFilter(
+        field_name="recall",
+        lookup_expr="gte",
+        label="Min Recall Score",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Min Recall Score"}
+        ),
+    )
+    recall_max = django_filters.NumberFilter(
+        field_name="recall",
+        lookup_expr="lte",
+        label="Max Recall Score",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Max Recall Score"}
+        ),
+    )
+    f1_score_min = django_filters.NumberFilter(
+        field_name="f1_score",
+        lookup_expr="gte",
+        label="Min F1 Score Score",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Min F1 Score Score"}
+        ),
+    )
+    f1_score_max = django_filters.NumberFilter(
+        field_name="f1_score",
+        lookup_expr="lte",
+        label="Max F1 Score Score",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Max F1 Score Score"}
+        ),
+    )
+    date_trained_start = django_filters.DateFilter(
+        field_name="date_trained",
+        lookup_expr="gte",
+        label="Date Start",
+        widget=DateInput(
+            attrs={"class": "form-control", "placeholder": "Submission Date Start"}
+        ),
+    )
+    date_trained_end = django_filters.DateFilter(
+        field_name="date_trained",
+        lookup_expr="lte",
+        label="Date End",
+        widget=DateInput(
+            attrs={"class": "form-control", "placeholder": "Submission Date End"}
+        ),
+    )
+    is_default = django_filters.ChoiceFilter(
+        field_name="is_default",
+        label="Is Active",
+        choices=[("", "All"), (True, "Yes"), (False, "No")],
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+
+    class Meta:
+        model = TrainedModel
+        fields = [
+            "name",
+            "version",
+            "model_type",
+            "accuracy_min",
+            "accuracy_max",
+            "precision_min",
+            "precision_max",
+            "recall_min",
+            "recall_max",
+            "f1_score_min",
+            "f1_score_max",
+            "date_trained_start",
+            "date_trained_end",
+            "is_default",
+        ]
+
+
+class ActivityLogFilter(django_filters.FilterSet):
+    user = django_filters.CharFilter(
+        field_name="user__username",
+        lookup_expr="icontains",
+        label="User",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Username"}
+        ),
+    )
+    action = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="Action",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Action"}
+        ),
+    )
+    ip_address = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="IP Address",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by IP Address"}
+        ),
+    )
+    user_agent = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="User Agent",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by User Agent"}
+        ),
+    )
+
+    # Submission Date Start field
+    timestamp_start = django_filters.DateFilter(
+        field_name="timestamp",
+        lookup_expr="gte",
+        label="Timestamp Date Start",
+        widget=DateInput(
+            attrs={"class": "form-control", "placeholder": "Timestamp Date Start"}
+        ),
+    )
+
+    # Timestamp Date End field
+    timestamp_end = django_filters.DateFilter(
+        field_name="timestamp",
+        lookup_expr="lte",
+        label="Timestamp Date End",
+        widget=DateInput(
+            attrs={"class": "form-control", "placeholder": "Timestamp Date End"}
+        ),
+    )
+
+    class Meta:
+        model = ActivityLog
+        fields = [
+            "user",
+            "action",
+            "ip_address",
+            "user_agent",
+            "timestamp_start",
+            "timestamp_end",
+        ]
+
+
+class FeedbackFilterFilter(django_filters.FilterSet):
+    user = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="User",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by User"}
+        ),
+    )
+    rating = django_filters.ChoiceFilter(
+        field_name="rating",
+        label="Rating",
+        choices=RATE_CHOICES,
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+    message = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="Message",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Search by Message"}
+        ),
+    )
+    submitted_at_start = django_filters.DateFilter(
+        field_name="submitted_at",
+        lookup_expr="gte",
+        label="Date Start",
+        widget=DateInput(
+            attrs={"class": "form-control", "placeholder": "Submission Date Start"}
+        ),
+    )
+    submitted_at_end = django_filters.DateFilter(
+        field_name="submitted_at",
+        lookup_expr="lte",
+        label="Date End",
+        widget=DateInput(
+            attrs={"class": "form-control", "placeholder": "Submission Date End"}
+        ),
+    )
+    show = django_filters.ChoiceFilter(
+        field_name="show",
+        label="Show",
+        choices=[("", "All"), (True, "Public"), (False, "Private")],
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+
+    class Meta:
+        model = Feedback
+        fields = [
+            "user",
+            "rating",
+            "message",
+            "submitted_at_start",
+            "submitted_at_end",
+            "show",
         ]
