b888b01243ab79d5b4f595f6b6a276f83309027e
zila-tech <info.zilatech@gmail.com>
2024-09-04 22:08:15 +0000

    Refactor path configurations for better organization

--- patients/urls.py
+++ patients/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path("questionnaire/", questionnaier, name="questionnaire"),
     path("questionnaire/update/<int:pk>/", questionnaier, name="update_questionnaire"),
     path("pending/results/", pending_result, name="pending_results"),
-    path(
-        "delete/pending-results/", pending_result_delete, name="delete_pending_result"
-    ),
+    path("delete/pending-results/", pending_result_delete, name="delete_pending_result"),
     path("summary/<pk>/", summary_view, name="summary"),
     path("result/<pk>/", results, name="result"),
     path("result-hostores/", result_hostores, name="result_hostores"),
