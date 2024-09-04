81c6f8f9ec3be1fb083c6361f038d23b1157edd7
zila-tech <info.zilatech@gmail.com>
2024-09-04 19:43:07 +0000

    Update utility functions log_user_activity to adhere to best practices

--- ml/utils.py
+++ ml/utils.py
@@ -3,16 +3,23 @@ from django.shortcuts import get_object_or_404
 from accounts.models import Account
 from ml.models import ActivityLog
 from decimal import Decimal
+import logging
+
+# Configure a logger instance
+logger = logging.getLogger("custom_logger")
 
 
-def log_user_activity(request,user, action):
-    if isinstance(user,Account):
-        ActivityLog.objects.create(
-            user=user,
-            action=action,
-            ip_address=request.META.get("REMOTE_ADDR"),
-            user_agent=request.META.get("HTTP_USER_AGENT"),
-        )
+def log_user_activity(request, user, action):
+    try:
+        if isinstance(user, Account):
+            ActivityLog.objects.create(
+                user=user,
+                action=action,
+                ip_address=request.META.get("REMOTE_ADDR"),
+                user_agent=request.META.get("HTTP_USER_AGENT"),
+            )
+    except Exception as e:
+        logger.error(f"Error logging user activity: {e}", exc_info=True)
 
 
 class DecimalEncoder(json.JSONEncoder):
