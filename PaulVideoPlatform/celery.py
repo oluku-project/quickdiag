19951ecb82c11f0d529613b95cd6a61d4dc89b64
zila-tech <info.zilatech@gmail.com>
2024-10-01 22:12:34 +0000

    Comment out code

--- PaulVideoPlatform/celery.py
+++ PaulVideoPlatform/celery.py
@@ -5,5 +5,16 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PaulVideoPlatform.settings")
 
 app = Celery("PaulVideoPlatform")
+
+# Using a string here means the worker doesn't need to serialize
+# the configuration object to child processes.
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+app.conf.update(
+    result_expires=3600,
+)
+
+app.conf.beat_scheduler = "django_celery_beat.schedulers:DatabaseScheduler"
